d0f93b1c092f623a6e6b4458d03c090b2495aa4c
yamachaaan <ymzk.yamachan@gmail.com>
2019-01-28 22:36:08 +0900

    read API_TOKEN at extra json file

--- todoist.py
+++ todoist.py
@@ -4,9 +4,17 @@
 from pytodoist import todoist
 import sys
 import argparse
+import json
 
+def load_json():
+    f = open('config.json', 'r')
+    json_data = json.load(f)
+    f.close()
+    return json_data['api_token']
 
-def main():
+def main():                                    
+    API_TOKEN = load_json()
+    print(API_TOKEN)
     user = todoist.login_with_api_token(API_TOKEN)
     today_tasks = user.search_tasks(todoist.Query.PRIORITY_1, todoist.Query.PRIORITY_2)
     for task in today_tasks:
