165c024b4550b622b90ff966083f107d152dd69d
yamachaaan <ymzk.yamachan@gmail.com>
2019-01-28 23:30:04 +0900

    divi Class

--- todoist.py
+++ todoist.py
@@ -2,23 +2,25 @@
 # _*_ coding:utf-8 _*_
 
 from pytodoist import todoist
-import sys
-import argparse
-import json
 
-def load_json():
-    f = open('config.json', 'r')
-    json_data = json.load(f)
-    f.close()
-    return json_data['api_token']
+class Main():
+    def load_json():
+        import json
+        f = open('config.json', 'r')
+        json_data = json.load(f)
+        f.close()
+        return json_data['api_token']
 
-def main():                                    
-    API_TOKEN = load_json()
-    print(API_TOKEN)
-    user = todoist.login_with_api_token(API_TOKEN)
-    today_tasks = user.search_tasks(todoist.Query.PRIORITY_1, todoist.Query.PRIORITY_2)
-    for task in today_tasks:
-        print(task.content + ' ' + str(task.id) + ' ' + str(task.due_date_utc) + ' ' + task.date_string)
+    def main(self):                                    
+        import sys
+        import argparse
+        API_TOKEN = Main.load_json()
+        user = todoist.login_with_api_token(API_TOKEN)
+        today_tasks = user.search_tasks(todoist.Query.PRIORITY_1, todoist.Query.PRIORITY_2)
+        for task in today_tasks:
+            print(task.content + ' ' + str(task.id) + ' ' + str(task.due_date_utc) + ' ' + task.date_string)
+    
 
 if __name__ == '__main__':
-    main()
+    main = Main()
+    main.main()
